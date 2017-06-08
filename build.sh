2e9e16f21274e0b1e8ff96ef439ba93f56acc530
Bruce Becker <brucellino@gmail.com>
2017-06-08 13:21:52 +0200

    change the url of the source file

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ mkdir -p ${SOFT_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's geet the source"
-  wget http://mirror.ufs.ac.za/gnuplot/${VERSION}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget https://downloads.sourceforge.net/project/${NAME}/${NAME}/${VERSION}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
